242335403db5992991a42b4526c509a6945b517c
Dominic Nidy <dominicnidy3@gmail.com>
2023-06-12 08:12:00 -0400

    Added feature to show password strength and estimated cracktimes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { RiFileCopyLine } from "react-icons/ri";
 import { BaseSyntheticEvent, useEffect, useState } from "react";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+var zxcvbn = require("zxcvbn");
 
 type PasswordOptionBtnProps = {
   labelText: string;
@@ -73,6 +75,9 @@ export default function Page() {
   const [isCopyPressed, setIsCopyPressed] = useState(false);
   const [generatedPassword, setGeneratedPassword] =
     useState<string>("P4$5W0rD!");
+  const [generatedPasswordStrength, setGeneratedPasswordStrength] = useState(0);
+  const [zxcvbnObject, setZxcvbnObject] = useState<any>(zxcvbn("P4$5W0rD!"));
+  const [showMoreDetails, setShowMoreDetails] = useState<boolean>(false);
 
   // Whenever password options change, ensure noOptionsSelected state adjusts accordingly
   useEffect(() => {
@@ -94,7 +99,10 @@ export default function Page() {
           Math.floor(Math.random() * availableCharacters.length)
         );
       }
+      const zxcvbnResult = zxcvbn(pass);
       setGeneratedPassword(pass);
+      setGeneratedPasswordStrength(zxcvbnResult.score);
+      setZxcvbnObject(zxcvbnResult);
     }
   }
 
@@ -125,15 +133,15 @@ export default function Page() {
     setIsCopyPressed(true);
     setTimeout(() => {
       setIsCopyPressed(false);
-    }, 111);
+    }, 165);
   };
 
   return (
-    <div className="flex min-h-screen w-screen flex-col items-center bg-grayish-gradient">
+    <div className="flex min-h-screen w-screen max-w-full flex-col items-center bg-grayish-gradient ">
       <h1 className="mt-12 pb-6 font-mono text-3xl font-semibold text-gray-400 sm:mt-12 md:mt-32 lg:mt-36">
         Password Generator
       </h1>
-      <div className="flex h-full w-5/6 flex-col gap-8 sm:w-auto md:w-[39rem] lg:w-[45rem]">
+      <div className="flex h-full w-5/6 flex-col gap-8 sm:w-auto md:w-[39rem] lg:w-[45rem] ">
         <div
           className="flex  h-24 w-full items-center justify-between rounded-sm bg-white bg-opacity-10 p-4 font-mono 
         text-4xl font-semibold tracking-wide text-white text-opacity-40 "
@@ -142,7 +150,7 @@ export default function Page() {
             placeholder={generatedPassword}
             className={`h-full w-5/6 text-ellipsis bg-white bg-opacity-0 outline-none ${
               isCopyPressed
-                ? "transition duration-100 placeholder:text-white placeholder:text-opacity-90"
+                ? "transition duration-100 placeholder:text-white placeholder:text-opacity-95"
                 : "bg-opacity-0 transition duration-200"
             }`}
             disabled
@@ -156,7 +164,7 @@ export default function Page() {
         </div>
 
         <div
-          className="flex h-auto w-full flex-col items-start gap-3 rounded-sm bg-white bg-opacity-10 
+          className="flex h-auto w-full flex-col items-start gap-3 rounded-sm bg-white bg-opacity-10
         p-4 font-mono text-xl font-semibold text-white text-opacity-70"
         >
           <PasswordLengthDragbar setPasswordLengthState={setPasswordLength} />
@@ -180,13 +188,102 @@ export default function Page() {
             state={includeSymbols}
             setState={setIncludeSymbols}
           />
+          <p
+            className="text-sm font-thin underline italic cursor-pointer"
+            onClick={() => setShowMoreDetails(!showMoreDetails)}
+          >
+            {showMoreDetails ? "Hide" : "Show more details"}
+          </p>
+
+          {showMoreDetails ? (
+            <div className="w-full rounded-sm p-1 text-sm font-bold bg-black bg-opacity-30 h-auto  cursor-default flex flex-col gap-2">
+              <div className="">
+                This password would take an estimated time of{" "}
+                <span className="bg-opacity-40 bg-black overflow-auto w-fit inline-flex">
+                  {Math.fround(
+                    zxcvbnObject?.crack_times_seconds
+                      ?.online_throttling_100_per_hour / 3.154e7
+                  ).toFixed(2)}
+                </span>{" "}
+                years to crack
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
+
+          {showMoreDetails ? (
+            <div className="bg-black bg-opacity-30 w-full rounded-sm p-1 pointer-events-none">
+              <div className="text-sm">
+                <div className="font-bold text-base mb-1">
+                  Cracktimes for different scenarios:
+                </div>
+                <div className="mb-1">
+                  Online, throttling, 0.027 hashes/sec:{" "}
+                  <span className="bg-opacity-30 bg-black w-fit">
+                    {
+                      zxcvbnObject?.crack_times_display
+                        ?.online_throttling_100_per_hour
+                    }
+                  </span>
+                </div>
+                <div className="mb-1">
+                  Online, no throttling, 10 hashes/sec:{" "}
+                  <span className="bg-opacity-30 bg-black w-fit">
+                    {
+                      zxcvbnObject?.crack_times_display
+                        ?.online_no_throttling_10_per_second
+                    }
+                  </span>
+                </div>
+                <div className="mb-1">
+                  Offline, 1e4 hashes/sec:{" "}
+                  <span className="bg-opacity-30 bg-black w-fit">
+                    {
+                      zxcvbnObject?.crack_times_display
+                        ?.offline_slow_hashing_1e4_per_second
+                    }
+                  </span>
+                </div>
+
+                <div className="mb-1">
+                  Offline, 1e10 hashes/sec:{" "}
+                  <span className="bg-opacity-30 bg-black w-fit">
+                    {
+                      zxcvbnObject?.crack_times_display
+                        ?.offline_fast_hashing_1e10_per_second
+                    }
+                  </span>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
+
           <div className="pointer-events-none flex h-auto w-full justify-between bg-black bg-opacity-40 p-3 pl-6 pr-6 text-3xl">
             <div>STRENGTH</div>
             <div className="flex gap-2">
-              <button className="h-full w-3 cursor-default border-2 border-white"></button>
-              <button className="h-full w-3 cursor-default border-2 border-white"></button>
-              <button className="h-full w-3 cursor-default border-2 border-white"></button>
-              <button className="h-full w-3 cursor-default border-2 border-white"></button>
+              <button
+                className={`h-full w-3 cursor-default border-2 border-white ${
+                  generatedPasswordStrength >= 1 ? "bg-white" : ""
+                } `}
+              ></button>
+              <button
+                className={`h-full w-3 cursor-default border-2 border-white ${
+                  generatedPasswordStrength >= 2 ? "bg-white" : ""
+                } `}
+              ></button>
+              <button
+                className={`h-full w-3 cursor-default border-2 border-white ${
+                  generatedPasswordStrength >= 3 ? "bg-white" : ""
+                } `}
+              ></button>
+              <button
+                className={`h-full w-3 cursor-default border-2 border-white ${
+                  generatedPasswordStrength >= 4 ? "bg-white" : ""
+                } `}
+              ></button>
             </div>
           </div>
           <div
